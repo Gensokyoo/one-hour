0e378a44d057f1ecd5e81e6a766ed7840cd65fec
vai <ncysatnaf@gmail.com>
2017-01-15 17:07:43 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import'ace-css/css/ace.css'
+import'font-awesome/css/font-awesome.css'
+
+import'./index.html'
+
+import Elm from './Main.elm'
+const mountNode = document.getElementById('main')
+
+const app = Elm.Main.embed(mountNode)
